5302d2eff1d09da7f654690068b0f1af6774843f
Mithun Halder <mithunrptc@gmail.com>
2019-03-29 00:40:17 +0600

    Add Category field

--- linkedin.py
+++ linkedin.py
@@ -21,14 +21,14 @@ email = browser.find_element_by_xpath('//*[@id="login-email"]')
 password = browser.find_element_by_xpath('//*[@id="login-password"]')
 submit = browser.find_element_by_xpath('//*[@id="login-submit"]')
 
-email.send_keys('')
-password.send_keys('')
+email.send_keys('mithun.tutorial.one@gmail.com')
+password.send_keys('mithun.tutorial.one')
 submit.click()
 time.sleep(2)
 
 # search_url = 'https://www.linkedin.com/search/results/companies/?keywords=Marketing%20and%20Advertising&origin=GLOBAL_SEARCH_HEADER&page={}'.format(i)
 
-for page in range(1, 10):
+for page in range(1, 3):
     print('Page Number {} processing.....'.format(page))
     search_url = 'https://www.linkedin.com/search/results/companies/?keywords=Marketing%20and%20Advertising&origin=GLOBAL_SEARCH_HEADER&page={}'.format(page)
     # search_url = str(search_url)
@@ -49,20 +49,21 @@ for page in range(1, 10):
                 location  = single_page_soup.find('div', class_="org-top-card-summary__info-item org-top-card-summary__headquarter").text.strip()
                 company_name= single_page_soup.find('h1', class_='org-top-card-summary__title')['title'].strip()
                 company_website = single_page_soup.find('span', class_="link-without-visited-state").text.strip()
-                company={'name':company_name, 'website': company_website, 'location': location}
+                category = single_page_soup.find('div', class_="org-top-card-summary__industry").text.strip()
+                company={'name':company_name, 'category':category, 'website': company_website, 'location': location}
                 data.append(company)
             except AttributeError:
                 pass
         except TypeError:
             pass
-    print('Page Number {} Completed'.format(1))
+    print('Page Number {} Completed'.format(page))
 
 
 def write_csv(data, filename="file2.csv"):
     with open(filename, 'w', newline='') as csvfile:
         f = csv.writer(csvfile)
         for line in data:
-            f.writerow([line['name'], line['location'], line['website']])
+            f.writerow([line['name'], line['category'], line['location'], line['website']])
 write_csv(data)
 
 
