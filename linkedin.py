7111746c300bfa3bfc625be59d0d61a37305279e
Mithun Halder <mithunrptc@gmail.com>
2019-03-28 22:20:19 +0600

    Browse Single Company

--- linkedin.py
+++ linkedin.py
@@ -28,14 +28,36 @@ time.sleep(2)
 
 # search_url = 'https://www.linkedin.com/search/results/companies/?keywords=Marketing%20and%20Advertising&origin=GLOBAL_SEARCH_HEADER&page={}'.format(i)
 
-for page in range(1, 10):
+for page in range(2, 3):
     search_url = 'https://www.linkedin.com/search/results/companies/?keywords=Marketing%20and%20Advertising&origin=GLOBAL_SEARCH_HEADER&page={}'.format(page)
     # search_url = str(search_url)
     browser.get(search_url)
     time.sleep(2)
     soup = BeautifulSoup(browser.page_source,'lxml')
     search_items = soup.find_all('li', class_='search-result__occluded-item')
-    
+
+    for item in search_items:
+        try:
+            company_url = item.find('a', class_='search-result__result-link')
+            company_url = company_url['href']
+            full_url = 'https://www.linkedin.com'+ company_url + 'about'
+            browser.get(full_url)
+            time.sleep(5)
+            single_page_soup = BeautifulSoup(browser.page_source, 'lxml')
+            location  = single_page_soup.find('div', class_="org-top-card-summary__info-item org-top-card-summary__headquarter").text
+            company_name= single_page_soup.find('a', class_='company-name-link').text 
+
+            company_website = single_page_soup.find('a', class_="link-without-visited-state")['href']
+
+            print(location, company_name, company_website)
+            break
+        except TypeError:
+            pass
+    print('-'*100)
+    break
+
+
+
     data.append(search_items)
 
 
